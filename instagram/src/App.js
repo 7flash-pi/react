a6a9e0d7dfb173c7c2f86d52849e73db4319582d
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-11-06 16:33:08 +0530

    intial

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,9 +1,19 @@
 
+import "./app.css";
 
 function App() {
   return (
-    <div className="App">
-      hii
+    <div className="app">
+
+      <div className="app_top">
+        <img src="https://www.freepnglogos.com/uploads/new-instagram-logo-with-transparent-background-9.png" alt="" className="app_logo" />
+      <h1>Reels</h1>
+      </div>
+
+      <div className="app_videos">
+        
+      </div>
+      
     </div>
   );
 }

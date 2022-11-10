c650909623dd8087dcb15612ed1193a49df73cfb
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-11-10 20:36:17 +0530

    new

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,62 @@
-import React, { useState } from 'react';
-import Todos from './components/Todos';
+import React, { useState, useEffect } from 'react';
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    return (list = JSON.parse(localStorage.getItem('list')));
+  } else {
+    return [];
+  }
+};
+
 
 
 function App() {
 
-  const [ todoList, setTodoList] = useState([]);
   const [task,setTask]= useState('');
+  const [list,setList]=useState([]);
 
-  const handleClick=(e)=>{
+
+  const removeTask=(id)=>{
+    const newList=list.filter((task)=> task.id===id )
+    setList(newList);
+  }
+
+  const addTask=(e)=>{
     e.preventDefault();
     if(task){
-      setTodoList([...todoList,task]);
+      const newtask={id:new Date().getTime().toString(),
+      task:task};
+      setList([...list,newtask]);
       setTask('');
     }
   }
+  useEffect(()=>{
+    localStorage.setItem('list', JSON.stringify(list));
+
+  },[list])
+
+  
   return (
    <div className="containe">
     <div className="todo-form">
       <input type="text" value={task} placeholder='Enter New Task' onChange={(e)=> setTask(e.target.value) } />
-      <button onClick={handleClick}>Add Task</button>
-      <Todos todoList={todoList}></Todos>
+      <button onClick={addTask}>Add Task</button>
+    </div>
+
+    <div className="todo-list">
+        {list.map((item) =>{
+            const {id,task}=item;
+            return  <div className="todo-row" key={id}>
+              <span>{task}</span>
+              
+                <button onClick={removeTask(id)}>Remove</button>
+
+    
+              </div>
+          })
+
+      }
     </div>
    </div>
   );

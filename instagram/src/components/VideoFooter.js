e5ed00df44da5b8734591e538a5faa3a8ec21276
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-11-19 13:57:14 +0530

    ticker add with bug :(

--- instagram/src/components/VideoFooter.js
+++ instagram/src/components/VideoFooter.js
@@ -3,7 +3,8 @@ import '../style/VideoFooter.css';
 
 import { Button, Avatar } from '@mui/material';
 import { MusicNote } from '@mui/icons-material';
-import Ticker from 'react-ticker';
+import Ticker ,{ NewsTicker } from 'nice-react-ticker';
+ 
 
 
 const VideoFooter = ({channel, avatarSrc, song, likes, shares}) => {
@@ -13,6 +14,14 @@ const VideoFooter = ({channel, avatarSrc, song, likes, shares}) => {
         <Avatar srcSet={avatarSrc}/>
         <h3>{channel} •<Button>Follow</Button></h3>
         
+      </div>
+      <div className="videoFooter_ticker">
+        <MusicNote className='videoFooter_icon'/>
+       <Ticker  isNewsTicker={true} className="ticker">
+        <NewsTicker id="1"  title={song}  />   
+        
+       </Ticker>
+
       </div>
       
     </div>

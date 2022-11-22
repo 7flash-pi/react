51087bd53d00443a1d94582144a80d9a54cd9d6e
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-11-22 22:53:25 +0530

    worjing on feed

---
+++ twitter-clone/src/components/Feed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../styles/feed.css"
+
+const Feed = () => {
+  return (
+    <div className='feed'>
+      <div className="feed_header">
+        <h2>Home</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Feed

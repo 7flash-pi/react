a0376e7b472c8adb9a45f8451c56939bce527beb
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-11-22 12:55:23 +0530

    sidebar done

---
+++ twitter-clone/src/components/Sidebar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../styles/sidebar.css'; 
+import TwitterIcon from '@mui/icons-material/Twitter';
+import SidebarOption from './SidebarOption';
+import HomeIcon from '@mui/icons-material/Home';   
+import SearchIcon from '@mui/icons-material/Search'; 
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import MailOutlinedIcon from '@mui/icons-material/MailOutlined';
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { Button } from '@mui/material';
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <TwitterIcon />
+        <SidebarOption active Icon={ HomeIcon } text='Home' />
+        <SidebarOption Icon={ SearchIcon } text='Explore'/>
+        <SidebarOption Icon={ NotificationsNoneIcon } text='Notifications' />  
+        <SidebarOption Icon={ MailOutlinedIcon  } text='Messages' />  
+        <SidebarOption Icon={ BookmarkBorderOutlinedIcon} text='BookMarks'/>
+        <SidebarOption Icon={ ListAltIcon } text='Lists'/>
+        <SidebarOption Icon={ PermIdentityIcon } text='Profile'/>
+        <SidebarOption Icon={ MoreHorizIcon } text='More'/>
+        
+        <Button variant="outlined" className='sidebar-tweet' fullWidth>Tweet</Button>
+
+      
+    </div>
+  )
+}
+
+export default Sidebar

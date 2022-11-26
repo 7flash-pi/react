24a24895e391f8488d4c193eb6202592aeddca0b
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-11-26 12:23:04 +0530

    widget partial

--- twitter-clone/src/components/Feed.js
+++ twitter-clone/src/components/Feed.js
@@ -12,7 +12,6 @@ const Feed = () => {
       </div>
       <TweetBox />
       <Post />
-
     </div>
   )
 }
--- twitter-clone/src/components/Widget.js
+++ twitter-clone/src/components/Widget.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import '../styles/Widget.css'
+import '../styles/Widget.css';
+import { Search } from '@mui/icons-material';
+import { TwitterTimelineEmbed, 
+TwitterShareButton,TwitterTweetEmbed } from 'react-twitter-embed';
 
 const Widget = () => {
   return (
     <div className='widget'>
-      <h2>Widget</h2>
+      <div className="widget__input">
+        <Search className='widget__searchIcon'/>  
+        <input type="text"
+        placeholder='Search Twitter' />  
+      </div>
+      <div className="widget__widgetContainer">
+        <h2>Whats Happening</h2>
+
+        <TwitterTweetEmbed tweetId={"1596365751476068353"} />
+
+        <TwitterTimelineEmbed sourceType='profile'
+        screenName='imVkohli'
+        options={{height:400}}  />
+
+        <TwitterShareButton
+          url={"https://facebook.com/cleverprogrammer"}
+          options={{ text: "#reactjs is awesome", via: "imVkohli" }}
+        />
+
+      </div>
+
     </div>
   )
 }
